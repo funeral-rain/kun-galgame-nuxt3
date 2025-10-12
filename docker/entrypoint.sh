c15db3f2295812262a7eee3baa1ce959f4ba3041
funeral-rain <nonlocal95@gmail.com>
2025-10-12 10:33:54 +0800

    feat: containerized deployment with Docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# shell will exit if any command returns a non-zero status
+set -e
+
+# generate .env file from .env.example
+CONFIG_DIR="/app/environment"
+HOST_ENV_FILE="$CONFIG_DIR/.env"
+EXAMPLE_ENV_FILE="/app/.env.example"
+APP_ENV_FILE="/app/.env"
+
+mkdir -p "$CONFIG_DIR"
+
+# if .env file not found on host， create one from .env.example file
+if [ ! -f "$HOST_ENV_FILE" ]; then
+  echo "Configuration file not found. Creating a new one from template..."
+  cp "$EXAMPLE_ENV_FILE" "$HOST_ENV_FILE"
+  echo "New configuration file created at $HOST_ENV_FILE"
+  echo "!!! Please review the file, update the variables, and then restart the container. !!!"
+  exit 0
+fi
+
+# create a symlink from the mounted config file to the app root directory
+echo "Linking configuration file..."
+ln -sf "$HOST_ENV_FILE" "$APP_ENV_FILE"
+
+
+# sync the database schema
+echo "Running database migrations..."
+
+pnpm exec prisma migrate deploy
+
+# start the application
+echo "Starting the application..."
+
+exec "$@"
